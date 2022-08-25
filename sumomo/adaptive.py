58aef48a0726556f1f17bbd67c8920fcd8911d10
atdurkin <alexdurkin96@gmail.com>
2022-08-25 15:38:50 +0100

    reformulate mod_ei_con

--- sumomo/adaptive.py
+++ sumomo/adaptive.py
@@ -112,7 +112,7 @@ class AdaptiveSampler:
         m.inputs = pyo.Var(m.n_inputs, bounds=self.space)
         # this is not flexible - a nn classifier outputs needs to pass through sigmoid
         m.feasibility_con = pyo.Constraint(expr= m.feas.outputs[0] >= 0.5 )
-        m.obj = pyo.Objective(expr=m.mdl.outputs[0], sense=pyo.minimize)
+        m.obj = pyo.Objective(expr=m.mdl.outputs[0], sense=pyo.maximize)
         
         # connect pyomo model input and output to the surrogate models
         m.c = pyo.ConstraintList()
@@ -131,7 +131,6 @@ class AdaptiveSampler:
         else:
             y_opt = np.min(y)
         constant_value = self.model.constant_value
-
         # initialise pyomo model
         m = pyo.ConcreteModel()
         m.mdl = pyo.Block(rule=BlockFormulation(self.model).rule())
@@ -150,9 +149,9 @@ class AdaptiveSampler:
             m.c.add( m.inputs[i] == m.feas.inputs[i] )
 
         m.mod_ei_con = pyo.Constraint(
-            expr= m.mod_ei ==
-            ((constant_value - m.mdl_std.outputs[0]) / (2 * 3.1416)) ** 0.5 * pyo.exp(
-                - (y_opt - m.mdl.outputs[0]) ** 2 / (2 * (constant_value - m.mdl_std.outputs[0]))
+            expr= m.mod_ei == pyo.sqrt(
+                (constant_value + m.mdl_std.outputs[0]) / (2 * 3.1416)) * pyo.exp(
+                    -(y_opt - m.mdl.outputs[0]) ** 2 / (2 * (constant_value + m.mdl_std.outputs[0]))
             )
         )
 
