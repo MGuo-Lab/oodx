2a4084b1a1fcb09222563a68166f60cc41d7da92
atdurkin <alexdurkin96@gmail.com>
2022-08-26 16:21:26 +0100

    fudge catching nn classifiers

--- sumomo/validator.py
+++ sumomo/validator.py
@@ -31,7 +31,7 @@ class Validator:
         self.r2 = None
         self.maxe = None
 
-    def calculate(self, x, y):
+    def calculate(self, x, y, nn_class=False):
         if self._is_classifier():
             prob_test, class_test = self.model.predict(x, return_class=True)
             self.accuracy = accuracy_score(y, class_test)
@@ -40,16 +40,24 @@ class Validator:
             self.f1 = f1_score(y, class_test)
             self.log_loss = log_loss(y, prob_test)
         else:
-            # note that these regression errors are in the scaled space
-            pred_test_ = self.model.predict(x)
-            pred_test = self.scaler.inv_scale_y(pred_test_)
-            self.mae = mean_absolute_error(y, pred_test)
-            self.mse = mean_squared_error(y, pred_test)
-            self.mape = mean_absolute_percentage_error(y, pred_test)
-            self.median_ae = median_absolute_error(y, pred_test)
-            self.evs = explained_variance_score(y, pred_test)
-            self.r2 = r2_score(y, pred_test)
-            self.maxe = max_error(y, pred_test)
+            if nn_class:
+                prob_test, class_test = self.model.predict(x, return_class=True)
+                self.accuracy = accuracy_score(y, class_test)
+                self.precision = precision_score(y, class_test)
+                self.recall = recall_score(y, class_test)
+                self.f1 = f1_score(y, class_test)
+                self.log_loss = log_loss(y, prob_test)
+            else:
+                # note that these regression errors are in the scaled space
+                pred_test_ = self.model.predict(x)
+                pred_test = self.scaler.inv_scale_y(pred_test_)
+                self.mae = mean_absolute_error(y, pred_test)
+                self.mse = mean_squared_error(y, pred_test)
+                self.mape = mean_absolute_percentage_error(y, pred_test)
+                self.median_ae = median_absolute_error(y, pred_test)
+                self.evs = explained_variance_score(y, pred_test)
+                self.r2 = r2_score(y, pred_test)
+                self.maxe = max_error(y, pred_test)
             
     def _is_classifier(self):
         # TODO - update this to enable NN classifiers to be caught too
