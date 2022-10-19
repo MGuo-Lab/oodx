aad90396ca2c5e0dc5b22f0915ce2053506dc780
atdurkin <alexdurkin96@gmail.com>
2022-10-19 16:53:18 +0100

    refactor nn formulations

--- sumomo/formulations.py
+++ sumomo/formulations.py
@@ -131,14 +131,13 @@ class BlockFormulation:
         b = self.model.biases
         
         # declare sets
-        layers = list(range(len(self.model.layers)))
+        m.layers = list(range(len(self.model.layers)))
         m.nodes = {layer: set(range(nodes)) for layer, nodes in enumerate(self.model.layers)}
-        m.N = set([(i, j) for i in m.nodes for j in m.nodes[i]])
         
         # declare variables
         m.inputs = pyo.Var(m.nodes[0])
-        m.z = pyo.Var(m.N)
-        m.a = pyo.Var(m.N)
+        m.z = pyo.Var(set([(i, j) for i in m.nodes for j in m.nodes[i]]))
+        m.a = pyo.Var(set([(i, j) for i in m.nodes for j in m.nodes[i]]))
         m.outputs = pyo.Var(m.nodes[len(self.model.layers) - 1])
 
         # constraints
@@ -147,7 +146,7 @@ class BlockFormulation:
         for n in m.nodes[1]:
             m.c.add(m.z[(1, n)] == sum(W[0][n, k] * m.inputs[k] for k in m.nodes[0]) + b[0][n])
         
-        for l in layers[1:]:
+        for l in m.layers[1:]:
             for n in m.nodes[l]:
                 m.c.add(m.z[(l, n)] == sum(W[l - 1][n, k] * m.a[(l - 1, k)] for k in m.nodes[l - 1]) + b[l - 1][n])
         
@@ -167,8 +166,9 @@ class BlockFormulation:
         m.c = pyo.ConstraintList()
       
         # activated layers return tanh of linear outputs
-        for l, n in m.N:
-            m.c.add( m.nn.a[(l, n)] == 1 - 2 / (pyo.exp(2 * m.nn.z[(l, n)]) + 1) )
+        for l in m.nn.layers[1:]:
+            for n in m.nn.nodes[l]:
+                m.c.add( m.nn.a[(l, n)] == 1 - 2 / (pyo.exp(2 * m.nn.z[(l, n)]) + 1) )
         
         # connect inputs to general model
         for i in m.nn.nodes[0]:
@@ -191,8 +191,9 @@ class BlockFormulation:
         m.c = pyo.ConstraintList()
       
         # activated layers return sigmoid of linear outputs
-        for l, n in m.nn.N:
-            m.c.add( m.nn.a[(l, n)] == 1 / (1 + pyo.exp(-m.nn.z[(l, n)])) )
+        for l in m.nn.layers[1:]:
+            for n in m.nn.nodes[l]:
+                m.c.add( m.nn.a[(l, n)] == 1 / (1 + pyo.exp(-m.nn.z[(l, n)])) )
         
         # connect inputs to general model
         for i in m.nn.nodes[0]:
@@ -215,8 +216,9 @@ class BlockFormulation:
         m.c = pyo.ConstraintList()
       
         # activated layers return softplus of linear outputs
-        for l, n in m.nn.N:
-            m.c.add( m.nn.a[(l, n)] == pyo.log(1 + pyo.exp(m.nn.z[(l, n)])) )
+        for l in m.nn.layers[1:]:
+            for n in m.nn.nodes[l]:
+                m.c.add( m.nn.a[(l, n)] == pyo.log(1 + pyo.exp(m.nn.z[(l, n)])) )
         
         # connect inputs to general model
         for i in m.nn.nodes[0]:
@@ -234,17 +236,18 @@ class BlockFormulation:
         # declare variables
         m.inputs = pyo.Var(m.nn.nodes[0])
         m.outputs = pyo.Var(m.nn.nodes[len(self.model.layers) - 1])
-        m.y = pyo.Var(m.nn.N, domain=pyo.Binary)
+        m.y = pyo.Var(set([(i, j) for i in m.nn.nodes for j in m.nn.nodes[i]]), domain=pyo.Binary)
 
         # constraints
         m.c = pyo.ConstraintList()
       
         # activated layers return ReLU of linear outputs, big-M formulation
-        for l, n in m.nn.N:
-            m.c.add( m.nn.a[(l, n)] >= 0 )
-            m.c.add( m.nn.a[(l, n)] >= m.nn.z[(l, n)] )
-            m.c.add( m.nn.a[(l, n)] <= 1e6 * m.y[(l, n)] )
-            m.c.add( m.nn.a[(l, n)] <= m.nn.z[(l, n)] + 1e6 * (1 - m.y[(l, n)]) )
+        for l in m.nn.layers[1:]:
+            for n in m.nn.nodes[l]:
+                m.c.add( m.nn.a[(l, n)] >= 0 )
+                m.c.add( m.nn.a[(l, n)] >= m.nn.z[(l, n)] )
+                m.c.add( m.nn.a[(l, n)] <= 1e6 * m.y[(l, n)] )
+                m.c.add( m.nn.a[(l, n)] <= m.nn.z[(l, n)] + 1e6 * (1 - m.y[(l, n)]) )
         
         # connect inputs to general model
         for i in m.nn.nodes[0]:
@@ -262,28 +265,29 @@ class BlockFormulation:
         # declare variables
         m.inputs = pyo.Var(m.nn.nodes[0])
         m.outputs = pyo.Var(m.nn.nodes[len(self.model.layers) - 1])
-        m.p = pyo.Var(m.nn.N, domain=pyo.Binary)
-        m.q = pyo.Var(m.nn.N, domain=pyo.Binary)
+        m.p = pyo.Var(set([(i, j) for i in m.nn.nodes for j in m.nn.nodes[i]]), domain=pyo.Binary)
+        m.q = pyo.Var(set([(i, j) for i in m.nn.nodes for j in m.nn.nodes[i]]), domain=pyo.Binary)
 
         # constraints
         m.c = pyo.ConstraintList()
       
         # activated layers return HardSigmoid of linear outputs, big-M formulation
-        for l, n in m.nn.N:
-            m.c.add( m.nn.a[(l, n)] <= m.p[(l, n)] )
-            m.c.add(
-                m.nn.a[(l, n)] >= 
-                m.nn.z[(l, n)] / 6 + 0.5 - 1e6 * (1 - m.p[(l, n)] + m.q[(l, n)])
-            )
-            m.c.add(
-                m.nn.a[(l, n)] <= 
-                m.nn.z[(l, n)] / 6 + 0.5 + 1e6 * (1 - m.p[(l, n)] + m.q[(l, n)])
-            )
-            m.c.add( m.nn.a[(l, n)] >= m.q[(l, n)] )
-            m.c.add( m.nn.z[(l, n)] - 1e6 * m.p[(l, n)] <= -3 )
-            m.c.add( m.nn.z[(l, n)] + 1e6 * (1 - m.p[(l, n)]) >= -3 )
-            m.c.add( m.nn.z[(l, n)] - 1e6 * m.q[(l, n)] <= 3 )
-            m.c.add( m.nn.z[(l, n)] + 1e6 * (1 - m.q[(l, n)]) >= 3 )
+        for l in m.nn.layers[1:]:
+            for n in m.nn.nodes[l]:
+                m.c.add( m.nn.a[(l, n)] <= m.p[(l, n)] )
+                m.c.add(
+                    m.nn.a[(l, n)] >= 
+                    m.nn.z[(l, n)] / 6 + 0.5 - 1e6 * (1 - m.p[(l, n)] + m.q[(l, n)])
+                )
+                m.c.add(
+                    m.nn.a[(l, n)] <= 
+                    m.nn.z[(l, n)] / 6 + 0.5 + 1e6 * (1 - m.p[(l, n)] + m.q[(l, n)])
+                )
+                m.c.add( m.nn.a[(l, n)] >= m.q[(l, n)] )
+                m.c.add( m.nn.z[(l, n)] - 1e6 * m.p[(l, n)] <= -3 )
+                m.c.add( m.nn.z[(l, n)] + 1e6 * (1 - m.p[(l, n)]) >= -3 )
+                m.c.add( m.nn.z[(l, n)] - 1e6 * m.q[(l, n)] <= 3 )
+                m.c.add( m.nn.z[(l, n)] + 1e6 * (1 - m.q[(l, n)]) >= 3 )
         
         # connect inputs to general model
         for i in m.nn.nodes[0]:
