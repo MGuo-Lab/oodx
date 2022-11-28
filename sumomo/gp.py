546d552bad5d4d86eaf3ab1a91e8511d832a5bc0
atdurkin <alexdurkin96@gmail.com>
2022-11-28 13:44:39 +0000

    test new GPC kernels

--- sumomo/gp.py
+++ sumomo/gp.py
@@ -119,7 +119,7 @@ class GPC:
             )
         sq_exp = self.sigma_f ** 2 * np.exp( - 0.5 / self.l ** 2 * sq_dist )
         
-        sq_exp = self.sigma_f ** 2 * (self.l ** 2 + sum(x1[:, j].reshape(1, -1) * x2[:, j].reshape(-1, 1) for j in range(x1.shape[1])))
+        sq_exp = self.sigma_f * (self.l ** 2 + sum(x1[:, j].reshape(1, -1) * x2[:, j].reshape(-1, 1) for j in range(x1.shape[1]))) ** 2
         
         return sq_exp
 
